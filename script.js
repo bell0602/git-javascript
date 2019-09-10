8ff11d8ae0110c5634861e6ae6340855e722362c
Brittany Bell <bell0602@algonquinlive.com>
2019-09-10 14:15:19 -0400

    Adds javascript

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const myName = 'Brittany Bell'
+var name = 'Brittany'
 
-console.log('Hello, my name is ')
+console.log(`Hello, my name is ${name}`)
+
+var number = 2
+
+console.log((((number * 2) * 5) / number) - 7)
